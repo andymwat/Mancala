03b2d7dc5d5ad3a273e42dc1682e0b9212e67a26
Andrew Watson <andrew.watson@ufl.edu>
2018-10-21 15:15:16 -0400

    Added board class

---
+++ Mancala/Mancala/Board.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "Board.h"
+
+
+Board::Board()
+{
+}
+
+
+Board::~Board()
+{
+}
---
+++ Mancala/Mancala/Board.h
@@ -0,0 +1,9 @@
+#pragma once
+class Board
+{
+public:
+	Board();
+	~Board();
+	int spots[14];
+};
+
